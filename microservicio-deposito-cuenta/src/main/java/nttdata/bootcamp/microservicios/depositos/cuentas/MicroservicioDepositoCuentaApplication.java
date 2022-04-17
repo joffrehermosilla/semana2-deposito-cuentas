209f91bf9bf963cf7f2f8d1f6f8815e18ea85ba8
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 17:23:11 -0500

    primer commit

---
+++ microservicio-deposito-cuenta/src/main/java/nttdata/bootcamp/microservicios/depositos/cuentas/MicroservicioDepositoCuentaApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.depositos.cuentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioDepositoCuentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioDepositoCuentaApplication.class, args);
+	}
+
+}
---
+++ microservicio-deposito-cuenta/src/test/java/nttdata/bootcamp/microservicios/depositos/cuentas/MicroservicioDepositoCuentaApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.depositos.cuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioDepositoCuentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
